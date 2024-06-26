fdc50c5c333671cc2b3bbf49f9284eea910f9915
Nicolas de Bressy <hermines.fichier0e@icloud.com>
2024-06-26 10:37:04 +0200

    delete old files

--- assets/script/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./select-elements"); // Assurez-vous que ce fichier est exécuté pour sélectionner les éléments
-require("./reveal-words"); // Assurez-vous que les mots sont révélés progressivement
-require("./parallax-scroll"); // Appliquez l'effet de parallaxe et gérez la classe 'scrolled' de la navbar
-require("./hover-bitcoin");
--- assets/script/main.ts
+++
@@ -1,4 +0,0 @@
-import "./select-elements"; // Assurez-vous que ce fichier est exécuté pour sélectionner les éléments
-import "./reveal-words"; // Assurez-vous que les mots sont révélés progressivement
-import "./parallax-scroll"; // Appliquez l'effet de parallaxe et gérez la classe 'scrolled' de la navbar
-import "./hover-bitcoin";
